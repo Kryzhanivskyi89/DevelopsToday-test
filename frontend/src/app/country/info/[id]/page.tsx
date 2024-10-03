dee37ec1de1bbeeaad60813459c5122223365860
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2024-10-03 13:51:41 +0300

    fix nextConfig

--- frontend/src/app/country/info/[id]/page.tsx
+++ frontend/src/app/country/info/[id]/page.tsx
@@ -1,3 +1,25 @@
+// import CountryInfo from "@/app/components/CountryInfo";
+// import axios from "axios";
+
+// interface Params {
+//   params: {
+//     id: string;
+//   };
+// }
+
+// export default async function CountryInfoContainer({ params }: Params) {
+//   const { id } = params;
+//   let countryInfo;
+//   let errorMessage = ""; 
+//   const response = await axios.get(
+//     `${process.env.NEXT_PUBLIC_COUNTRY_INFO_URL}${id}`
+//   );
+//   countryInfo = response.data.info;  
+
+//   return <CountryInfo countryInfo={countryInfo} errorMessage={errorMessage} />;
+  
+// }
+
 import CountryInfo from "@/app/components/CountryInfo";
 import axios from "axios";
 
@@ -7,17 +29,31 @@ interface Params {
   };
 }
 
+// Функція для генерації статичних параметрів
+export async function generateStaticParams() {
+  // Припустимо, що у вас є список ідентифікаторів країн
+  const ids = ["1", "2", "3"]; // Замініть це на ваші дані або отримайте з API
+
+  return ids.map(id => ({
+    id,
+  }));
+}
+
 export default async function CountryInfoContainer({ params }: Params) {
   const { id } = params;
   let countryInfo;
   let errorMessage = ""; 
-  const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_COUNTRY_INFO_URL}${id}`
-  );
-  countryInfo = response.data.info;  
+
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_COUNTRY_INFO_URL}${id}`
+    );
+    countryInfo = response.data.info;  
+  } catch (error) {
+    errorMessage = "Error fetching country information.";
+  }
 
   return <CountryInfo countryInfo={countryInfo} errorMessage={errorMessage} />;
-  
 }
 
 // export default async function CountryInfoContainer({ params }: Params) {
