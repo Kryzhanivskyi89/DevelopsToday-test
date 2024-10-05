c57946124fafbea666ff279691af9746642291f0
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2024-10-05 11:38:25 +0300

    move to root

--- backend/src/config/config.js
+++
@@ -1,9 +0,0 @@
-import { configDotenv } from "dotenv";
-
-configDotenv({ override: true, path: "./.env" });
-
-const config = {
-  PORT: process.env.PORT,
-};
-
-export default config;
--- backend/src/services/errors/CustomError.js
+++
@@ -1,9 +0,0 @@
-export default class CustomError {
-  static createError({ name, cause, code }) {
-    let error = Error(cause);
-    error.name = name;
-    error.description = cause;
-    error.code = code;
-    throw error;
-  }
-}
